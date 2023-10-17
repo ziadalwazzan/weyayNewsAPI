386026f0513a44d409cd8e127a841748346a5d02
Ziad AlWazzan <51064118+ziadalwazzan@users.noreply.github.com>
2023-10-17 09:56:43 +0300

    Client Bean mod + accommodations

--- src/main/kotlin/com/example/weyayNewsAPI/dto/SourcesDto.kt
+++ src/main/kotlin/com/example/weyayNewsAPI/dto/SourcesDto.kt
@@ -1,8 +1,6 @@
 package com.example.weyayNewsAPI.dto
 
 import com.example.weyayNewsAPI.models.Source
-import com.fasterxml.jackson.annotation.JsonProperty
-
 
 class SourcesDTO {
     var status: String? = null
